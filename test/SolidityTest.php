fc2a702aa6938997402ba770030fd5749678d23f
Boris Momčilović <boris@firstbeatmedia.com>
2017-12-05 13:27:12 +0100

    Restructure test

--- test/SolidityTest.php
+++ test/SolidityTest.php
@@ -13,9 +13,16 @@ class SolidityTest extends PHPUnit\Framework\TestCase
     private const nonce = 5;
     private const expires = '10000';
 
-    public function testSha3()
+    /**
+     * @dataProvider sha3
+     */
+    public function testSha3($data, $expectation)
     {
-        $expect = [
+        $this->assertEquals(Solidity::sha3($data), $expectation);
+    }
+
+    public static function sha3 (): array {
+        return [
             [self::contractAddress, '0x9f13f88230a70de90ed5fa41ba35a5fb78bc55d11cc9406f17d314fb67047ac7'],
             [self::tokenBuy, '0x5380c7b7ae81a58eb98d9c78de4a1fd7fd9535fc953ed2be602daaa41767312a'],
             [self::amountBuy, '0xfb05e4134e5b30db022b94b822e7d19b1e5cd1c244468eada63789fd3514454a'],
@@ -25,12 +32,9 @@ class SolidityTest extends PHPUnit\Framework\TestCase
             [self::nonce, '0x036b6384b5eca791c62761152d0c79bb0604c104a5fb6f4eb0703f3154bb3db0'],
             [self::address, '0x5c72003ad77a34d6c7061c57eb81dd46bc248e43cfd5bd64fb43f10c2edb805b'],
         ];
+    }
 
-        foreach ($expect as $test) {
-            $this->assertEquals(Solidity::sha3($test[0]), $test[1]);
-        }
-
+    public function testSha3Variadic() {
         $this->assertEquals(Solidity::sha3(self::contractAddress, self::tokenBuy, self::amountBuy, self::tokenSell, self::amountSell, self::expires, self::nonce, self::address), '0xf20f20d357419f696f69e6ff05bc6566b1e6d38814ce4f489d35711e2fd2c481');
-
     }
 }
